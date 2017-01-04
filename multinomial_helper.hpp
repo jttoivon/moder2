8133e3c2c5b58738f8a686fb09a9ff588b79a0dd
Jarkko Toivonen <jttoivon@cs.helsinki.fi>
2017-01-04 16:32:50 +0200

    Added license to all source files

--- multinomial_helper.hpp
+++ multinomial_helper.hpp
@@ -1,3 +1,23 @@
+/*
+
+    MODER is a program to learn DNA binding motifs from SELEX datasets.
+    Copyright (C) 2016, 2017  Jarkko Toivonen
+
+    MODER is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    MODER is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
 #include "matrix.hpp"
 #include "suffix_array_wrapper.hpp"
 
