5553c72158c95e0280b1d2815e4b2f4e818bb05b
Jarkko Toivonen <jttoivon@cs.helsinki.fi>
2018-01-22 23:47:10 +0200

    Added a missing file

---
+++ CPM03/dependencies.mk
@@ -0,0 +1,3 @@
+test-suffixsort.o: test-suffixsort.cpp suffixsort.hpp doubling.hpp \
+ partition.hpp stringsort.hpp checker.hpp difference_cover.hpp timing.hpp
+difference_cover.o: difference_cover.cpp difference_cover.hpp
