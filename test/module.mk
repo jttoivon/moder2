04782018335159eb306404f56621f68676d0f008
Jarkko Toivonen <jttoivon@cs.helsinki.fi>
2019-01-03 11:23:15 +0200

    Added missing test files

---
+++ test/module.mk
@@ -0,0 +1,78 @@
+##########################################
+#
+# test programs
+#
+##########################################
+
+TEST_PROGRAMS2=test_suffix_array_wrapper test_kmer_tools test_packed_string test_dinucleotide test_multinomial
+TEST_PROGRAMS=$(addprefix test/, $(TEST_PROGRAMS2))
+
+
+TESTCXXFLAGS=$(CXXFLAGS) -I.
+TESTLDFLAGS=$(LDFLAGS) -lboost_unit_test_framework
+
+.PHONY: test_individual
+test_individual: $(TEST_PROGRAMS)
+	for p in $(TEST_PROGRAMS) ; do echo Running $$p; ./$$p ; done
+
+.PHONY: test
+test: test/test_main
+	test/test_main
+
+.PHONY: test_clean
+test_clean:
+	rm -f test/*.o $(TEST_PROGRAMS)
+
+
+test/%.o: test/%.cpp
+	$(CXX) $(TESTCXXFLAGS) -c $< -o $@
+
+
+##########################################
+#
+# single executable
+#
+##########################################
+
+# TEST_ALL_OBJS=dinucleotide.o iupac.o common.o data.o parameters.o probabilities.o my_assert.o multinomial_helper.o suffix_array_wrapper.o matrix_tools.o orientation.o kmer_tools.o CPM03/difference_cover.o combinatorics.o seed_basic.o packed_string.o huddinge.o
+# TEST_ALL_SOURCES=test/test_main.cpp test/test_dinucleotide.cpp test/test_suffix_array_wrapper.cpp test/test_multinomial.cpp test/test_kmer_tools.cpp test/test_packed_string.cpp
+# test/test_main: $(TEST_ALL_OBJS) $(TEST_ALL_SOURCES:.cpp=.o)
+# 	$(CXX) $(TESTCXXFLAGS) $+ -o test/test_main  $(TESTLDFLAGS)
+
+TEST_ALL_OBJS=dinucleotide.o iupac.o common.o data.o parameters.o probabilities.o my_assert.o multinomial_helper.o suffix_array_wrapper.o matrix_tools.o orientation.o kmer_tools.o CPM03/difference_cover.o combinatorics.o huddinge.o bndm.o
+TEST_ALL_SOURCES=test/test_main.cpp test/test_dinucleotide.cpp
+test/test_main: $(TEST_ALL_OBJS) $(TEST_ALL_SOURCES:.cpp=.o)
+	$(CXX) $(TESTCXXFLAGS) $+ -o test/test_main  $(TESTLDFLAGS)
+
+
+
+##########################################
+#
+# multiple executables
+#
+##########################################
+
+
+TEST_DINUCLEOTIDE_OBJS=dinucleotide.o iupac.o common.o data.o parameters.o probabilities.o my_assert.o multinomial_helper.o suffix_array_wrapper.o matrix_tools.o orientation.o kmer_tools.o CPM03/difference_cover.o combinatorics.o
+test/test_dinucleotide: $(TEST_DINUCLEOTIDE_OBJS) test/test_dinucleotide.cpp
+	$(CXX) -DSTAND_ALONE $(TESTCXXFLAGS) $+ -o $@ $(TESTLDFLAGS)
+
+
+# TEST_SUFFIX_ARRAY_WRAPPER_OBJS=CPM03/difference_cover.o suffix_array_wrapper.o iupac.o common.o orientation.o
+# test/test_suffix_array_wrapper: $(TEST_SUFFIX_ARRAY_WRAPPER_OBJS) test/test_suffix_array_wrapper.cpp
+# 	$(CXX) -DSTAND_ALONE $(TESTCXXFLAGS) $+ -o $@ $(TESTLDFLAGS)
+
+# TEST_KMER_TOOLS_OBJS=kmer_tools.o common.o orientation.o iupac.o data.o probabilities.o parameters.o my_assert.o \
+#                      combinatorics.o
+# test/test_kmer_tools: $(TEST_KMER_TOOLS_OBJS) test/test_kmer_tools.cpp
+# 	$(CXX) -DSTAND_ALONE  $(TESTCXXFLAGS) $+ -o $@ $(TESTLDFLAGS)
+
+# TEST_PACKED_STRING_OBJS=packed_string.o common.o orientation.o iupac.o data.o probabilities.o parameters.o my_assert.o combinatorics.o
+# test/test_packed_string: $(TEST_PACKED_STRING_OBJS) test/test_packed_string.cpp
+# 	$(CXX) -DSTAND_ALONE $(TESTCXXFLAGS) $+ -o $@ $(TESTLDFLAGS)
+
+
+
+# TEST_MULTINOMIAL_OBJS=dinucleotide.o  iupac.o common.o data.o parameters.o probabilities.o my_assert.o multinomial_helper.o suffix_array_wrapper.o matrix_tools.o orientation.o kmer_tools.o CPM03/difference_cover.o combinatorics.o
+# test/test_multinomial: $(TEST_MULTINOMIAL_OBJS) test/test_multinomial.cpp
+# 	$(CXX) -DSTAND_ALONE  $(TESTCXXFLAGS) $+ -o $@ $(TESTLDFLAGS)
---
+++ test/test_dinucleotide.cpp
@@ -0,0 +1,249 @@
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#define BOOST_TEST_MODULE Main
+#endif
+
+#include <boost/test/unit_test.hpp>
+//#include <boost/test/included/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+#include <boost/mpl/list.hpp>
+
+#include <cassert>
+#include <vector>
+#include <ostream>
+#include <fstream>
+#include <libgen.h>
+
+#include "dinucleotide.hpp"
+
+
+dinuc_model<double>
+adm_from_background(const std::vector<double>& bg, int k)
+{
+  assert(k >= 1);
+  dmatrix result(16, k);
+  dmatrix matrix_bg(4,1);
+  for (int b=0; b < 4; ++b)
+    matrix_bg(b, 0) = bg[b];
+      
+  for (int j=0; j < k; ++j) {
+    int amax = j==0 ? 1 : 4;
+    for (int a=0; a < amax; ++a) {
+      result.inject(matrix_bg, 4*a, j);
+    }
+  }
+
+  dinuc_model<double> adm;
+  adm.init(result);
+  return adm;
+}
+
+dinuc_model<double>
+random_adm(int k)
+{
+  assert(k >= 1);
+  dmatrix result(16, k);
+
+  int seed = 0;
+  //std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  //  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::mt19937 gen(seed); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+  dmatrix m(4,1);
+  
+  for (int j=0; j < k; ++j) {
+    int amax = j==0 ? 1 : 4;
+    for (int a=0; a < amax; ++a) {
+      for (int b=0; b < 4; ++b)
+	m(b, 0) = dis(gen);
+      normalize_matrix_columns(m);
+      result.inject(m, 4*a, j);
+    }
+  }
+
+  dinuc_model<double> adm(result);
+  return adm;
+}
+
+// Does not work
+// template <typename A, typename B>
+// std::ostream&
+// operator<<(std::ostream& str, std::pair<A, B> p)
+// {
+//   str << "<" << p.first << "," << p.second << ">";
+
+//   return str;
+// }
+
+//typedef double FloatType;
+
+// see https://stackoverflow.com/questions/10976130/boost-check-equal-with-pairint-int-and-custom-operator
+namespace boost
+{
+    namespace test_tools
+    {
+        template<typename T,typename U>
+        struct print_log_value<std::pair<T, U> >
+        {
+            void operator()(std::ostream& os, std::pair<T, U> const& pr)
+            {
+                os << "<" << std::get<0>(pr) << "," << std::get<1>(pr) << ">";
+            }
+        };
+    }
+}
+
+struct F {
+  F() : hoxb13(get_model_filename())       { BOOST_TEST_MESSAGE( "setup fixture" ); }
+  ~F()         { BOOST_TEST_MESSAGE( "teardown fixture" ); }
+
+  dinuc_model<double> hoxb13;
+  
+private:
+
+  std::string
+  get_model_filename() const
+  {
+    std::string temp(boost::unit_test::framework::master_test_suite().argv[0]);
+    std::string dir = dirname((char*)temp.c_str());
+    //printf("argv[0]: %s\n", temp.c_str());
+    //printf("dirname(argv[0]): %s\n", dir.c_str());
+    return dir + "/HOXB13-CYMRTAAAA.adm";
+  }
+  
+};
+
+//____________________________________________________________________________//
+
+
+BOOST_FIXTURE_TEST_SUITE( dinucleotide, F )
+
+BOOST_AUTO_TEST_CASE(test_sub_adm)
+{
+  int k = 9;
+  dinuc_model<double> adm = hoxb13;
+  BOOST_CHECK_EQUAL(adm.dim(), std::make_pair(16, k));
+
+  dinuc_model<double> another_adm = sub_adm(adm, 0, k);
+  BOOST_CHECK_EQUAL(adm, another_adm);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_left_extend_adm)
+{
+  int k = 9;
+  dinuc_model<double> adm = hoxb13;
+  std::vector<double> bg(4, 0.25);
+  
+  for (int extension=0; extension < 10; ++extension) {
+
+    dinuc_model<double> another_adm = left_extend_adm(adm, bg, extension);
+    BOOST_CHECK_EQUAL(another_adm.dim(), std::make_pair(16, k+extension));
+    BOOST_CHECK_EQUAL(adm, sub_adm(another_adm, extension, k));
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_right_extend_adm)
+{
+  int k = 9;
+  dinuc_model<double> adm = hoxb13;
+  std::vector<double> bg(4, 0.25);
+  
+  for (int extension=0; extension < 10; ++extension) {
+
+    dinuc_model<double> another_adm = right_extend_adm(adm, bg, extension);
+    BOOST_CHECK_EQUAL(another_adm.dim(), std::make_pair(16, k+extension));
+    BOOST_CHECK_EQUAL(adm, sub_adm(another_adm, 0, k));
+  }
+}
+
+
+BOOST_AUTO_TEST_CASE(test_reverse_complement)
+{
+  dinuc_model<double> adm = hoxb13;
+
+  dinuc_model<double> another_adm = reverse_complement(adm);
+  
+  BOOST_CHECK_EQUAL(adm, reverse_complement(another_adm));
+
+}
+
+BOOST_AUTO_TEST_CASE(test_combination)
+{
+  int k = 9;
+  int extension = 5;
+  std::vector<double> bg(4, 0.25);
+  
+  dinuc_model<double> adm = hoxb13;
+
+  dinuc_model<double> another_adm = left_extend_adm(adm, bg, extension);
+  another_adm = reverse_complement(another_adm);
+  another_adm = sub_adm(another_adm, 0, k);
+  another_adm = reverse_complement(another_adm);
+  BOOST_CHECK_EQUAL(adm, another_adm);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_force_adms_equal)
+{
+  int k = 9;
+  std::vector<double> bg(4, 0.25);
+  
+  dinuc_model<double> adm = hoxb13;
+
+  dinuc_model<double> background_adm = adm_from_background(bg, k);
+  dinuc_model<double> another_adm = force_adms_equal(adm, background_adm);
+  BOOST_CHECK_EQUAL(adm, another_adm);
+  
+}
+
+BOOST_AUTO_TEST_CASE(test_representation)
+{
+  
+  dinuc_model<double> adm = hoxb13;
+  int k = adm.get_length();
+  
+  dmatrix rep = adm.representation();
+  BOOST_CHECK_EQUAL(rep.dim(), std::make_pair(16, k));
+  for (int a=4; a < 16; ++a)
+    BOOST_CHECK_EQUAL(rep(a,0), 0.0);
+  dinuc_model<double> adm2 = dinuc_model<double>(rep);
+  BOOST_CHECK_EQUAL(adm, adm2);
+}
+
+BOOST_AUTO_TEST_CASE(test_probability)
+{
+  
+  dinuc_model<double> adm = hoxb13;
+  dinuc_model<double> rev = reverse_complement(adm);
+  int k = adm.get_length();
+
+  for (int code=0; code < pow(4, k); ++code) {
+    std::string s = number_to_dna(code, k);
+    double p1 = rev.probability(s);
+    double p2 = adm.probability(reverse_complement(s));
+    BOOST_CHECK_CLOSE(p1, p2, 0.0001);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_cut_and_join)
+{
+  
+  dinuc_model<double> adm = hoxb13;
+  int k = adm.get_length();
+
+  for (int j=1; j < k; ++j) {
+    dinuc_model<double> left = sub_adm(adm, 0, j);
+    dinuc_model<double> right = sub_adm(adm, j-1, k-j+1);
+    BOOST_CHECK_EQUAL(left.get_length(), j);
+    BOOST_CHECK_EQUAL(right.get_length(), k-j+1);
+    dinuc_model<double> join = join_adms(left, right);
+    BOOST_CHECK_EQUAL(adm, join);
+  }
+  
+}
+
+//____________________________________________________________________________//
+
+BOOST_AUTO_TEST_SUITE_END()
